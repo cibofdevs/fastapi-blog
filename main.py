fc5b822d78bbcd200d7a4ca1c26c7a038a64078a
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-09 10:01:24 +0700

    add mount static file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from db import models
 from db.database import engine
 from router import post
@@ -6,4 +7,8 @@ from router import post
 app = FastAPI()
 app.include_router(post.router)
 
+# generate database
 models.Base.metadata.create_all(engine)
+
+# mount static file
+app.mount("/images", StaticFiles(directory="images"), name="images")
