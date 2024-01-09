3f2ce037344fe36054bf3def923f77e33d74c5b9
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-09 08:34:25 +0700

    add database model

---
+++ db/models.py
@@ -0,0 +1,13 @@
+from db.database import Base
+from sqlalchemy import Column, Integer, String, DateTime
+
+
+class DbPost(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True, index=True)
+    image_url = Column(String)
+    title = Column(String)
+    content = Column(String)
+    creator = Column(String)
+    timestamp = Column(DateTime)
+
