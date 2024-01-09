27fb7a4996662b547b6317412755496d6919b802
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-09 09:08:32 +0700

    add create post database operation

---
+++ db/db_post.py
@@ -0,0 +1,19 @@
+import datetime
+
+from router.schemas import PostBase
+from sqlalchemy.orm.session import Session
+from db.models import DbPost
+
+
+def create(db: Session, request: PostBase):
+    new_post = DbPost(
+        image_url=request.image_url,
+        title=request.title,
+        content=request.content,
+        creator=request.creator,
+        timestamp=datetime.datetime.now()
+    )
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)  # capture generated id
+    return new_post
