a7a754c5b0c26c4766ce6b971aee47e18c0693c3
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-09 10:09:32 +0700

    add cors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from db import models
 from db.database import engine
 from router import post
@@ -12,3 +13,16 @@ models.Base.metadata.create_all(engine)
 
 # mount static file
 app.mount("/images", StaticFiles(directory="images"), name="images")
+
+# cors
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
