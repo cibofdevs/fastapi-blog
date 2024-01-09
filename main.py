403a256e736503667ccba0bf860cb6d2c8abe6fa
Ahmad Wijaya <cibofdev@gmail.com>
2024-01-09 09:09:09 +0700

    include post router to main

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 from db import models
 from db.database import engine
+from router import post
 
 app = FastAPI()
-
-
-@app.get("/")
-def hello_world():
-    return "Hello World!"
-
+app.include_router(post.router)
 
 models.Base.metadata.create_all(engine)
